2f2d59287315b3c5ed43c888d6cdd3fcfc20d6d1
qithron <anonyrise@gmail.com>
2022-09-03 00:24:16 +0700

    add script "are-you-root", some changes

--- asd/.sh
+++ asd/.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 #
-# shared config
+# shared config for my shells (bash, dash, fish)
 
 ###############################################################################
 # environment
@@ -10,18 +10,25 @@ export  XDG_CACHE_HOME="$HOME/.cache"
 export   XDG_DATA_HOME="$HOME/.local/share"
 export  XDG_STATE_HOME="$HOME/.local/state"
 
+export EDITOR='nvim'
+export VISUAL="$EDITOR"
+
+echo "$PATH" | grep -q "$HOME/.local/bin:$HOME/.local/sbin:" ||
+    export PATH="$HOME/.local/bin:$HOME/.local/sbin:$PATH"
+
 export HISTCONTROL='ignoreboth:erasedups'
 export HISTFILESIZE=
 export HISTSIZE=
 export HISTIGNORE='rm *:ls:cd:echo:e:vim:pkill *'
 
-export PATH="$HOME/.local/bin:$HOME/.local/sbin:$PATH" # sbin = secret bin :)
-export EDITOR='nvim'
-export VISUAL="$EDITOR"
 export PYTHONDONTWRITEBYTECODE='1'
-export PYTHONPATH="$HOME/.local/lib/python/site-packages:$PYTHONPATH"
+echo "$PYTHONPATH" | grep -q "$HOME/.local/lib/python/site-packages:" ||
+    export PYTHONPATH="$HOME/.local/lib/python/site-packages:$PYTHONPATH"
+
 export CARGO_HOME="$HOME/.local/share/cargo"
 
+# TODO: considering to make a special directory
+# TODO:     to be sourced instead modify this file
 export RANGER_TMP="$(ranger-env)"
 
 export GIT_PAGER='cat'
@@ -34,7 +41,6 @@ alias ls='ls -lA --color=auto --group-directories-first'
 alias grep='grep --color=auto'
 
 [ -x '/usr/bin/pacman' ] && alias pacman='pacman --verbose --color=auto'
-[ -x '/usr/bin/sudo' ] && alias sudoer='/usr/bin/sudo'
 
 alias reload-gpgAgent='echo RELOADAGENT | gpg-connect-agent'
 alias aplay-noise='aplay -t raw /dev/random -d 0 -r 4000 -c 2 -N'
@@ -50,3 +56,8 @@ alias cdbspwm='cd ~/.config/bspwm'
 alias cdfish='cd ~/.config/fish'
 alias cdnvim='cd ~/.config/nvim'
 alias cdportage='cd /etc/portage'
+
+# git
+alias gitd='git diff'
+alias gitl='git log'
+alias gits='git status'
